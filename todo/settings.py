a11e0418d67209820db60b6534c191a4865f83e0
Dharmi Chavda <70809100+dharmi158@users.noreply.github.com>
2021-04-02 20:30:58 +0530

    Update settings.py

--- todo/settings.py
+++ todo/settings.py
@@ -111,3 +111,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'feni158chavda@gmail.com'
+EMAIL_HOST_PASSWORD = 'feni696969'
